7105b72314ee31cc0bfc0113c511230c98914971
Maxim Mordasov <maskin.m25@gmail.com>
2025-10-09 00:44:18 +0300

    Upd version

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,5 @@
 export function renderCaptions(ctx: CanvasRenderingContext2D, text: string) {
-  ctx.font = "24px sans-serif";
+  ctx.font = "48px sans-serif";
   ctx.fillStyle = "#17c499ff";
   ctx.fillText(text, 150, 50);
   return true;
