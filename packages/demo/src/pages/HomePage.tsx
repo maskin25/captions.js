44b2ecb64206bdee79a9ee6848d5fc1598859d56
Maksim Mordasov <maskin.m25@gmail.com>
2025-11-09 12:46:10 +0300

    Change featured example

--- packages/demo/src/pages/HomePage.tsx
+++ packages/demo/src/pages/HomePage.tsx
@@ -4,17 +4,32 @@ function HomePage() {
   return (
     <div className="p-4 flex flex-col items-center gap-4">
       <ExampleFeatured
+        videoSrc="https://storage.googleapis.com/shorty-uploads/captions.js/rKpltaOMFdc.mp4"
+        captionsSrc="https://storage.googleapis.com/shorty-uploads/captions.js/rKpltaOMFdc-dg.json"
+        presetName="From"
+      />
+      {/* <ExampleFeatured
+        videoSrc="https://storage.googleapis.com/shorty-uploads/captions.js/Mf7HyLkuNS0.mp4"
+        captionsSrc="https://storage.googleapis.com/shorty-uploads/captions.js/Mf7HyLkuNS0-dg.json"
+        presetName="From"
+      /> */}
+      {/*   <ExampleFeatured
+        videoSrc="https://storage.googleapis.com/shorty-uploads/captions.js/0DOMlNJ9sOk.mp4"
+        captionsSrc="https://storage.googleapis.com/shorty-uploads/captions.js/0DOMlNJ9sOk-dg.json"
+        presetName="From"
+      /> */}
+      {/*  <ExampleFeatured
         videoSrc="https://storage.googleapis.com/shorty-uploads/matthew.mp4"
         //captionsSrc="https://storage.googleapis.com/shorty-uploads/matthew-plain.json"
         captionsSrc="https://storage.googleapis.com/shorty-uploads/matthew-dg.json"
         presetName="From"
-      />
-      <ExampleFeatured
+      /> */}
+      {/*  <ExampleFeatured
         videoSrc="https://storage.googleapis.com/shorty-uploads/margo.mp4"
         //captionsSrc="https://storage.googleapis.com/shorty-uploads/margo-plain.json"
         captionsSrc="https://storage.googleapis.com/shorty-uploads/margo-dg.json"
         presetName="Lovly"
-      />
+      /> */}
     </div>
   );
 }
